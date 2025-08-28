dad8d9120f3b8793fe0df427a32ed2b66f37406c
paul-96-shimu <shimupaul96@gmail.com>
2025-08-28 18:20:27 +0600

    added all work

--- src/Components/Performance.jsx
+++ src/Components/Performance.jsx
@@ -1,10 +1,102 @@
 import React from 'react';
 
-import image from '../assets/image 2499.png'
+import image from '../assets/Bg.png'
+
+import image1 from '../assets/orange circleImage.png'
+
+import image0 from '../assets/double-quote-left.png'
+
+import image2 from '../assets/black Ellipse 881.png'
+
+import image3 from '../assets/Vector 1234 underline.png'
+
+import image4 from '../assets/Slider Button orange.png'
+import image5 from '../assets/image 2495@2x.png'
+
 const Performance = () => {
     return (
-        <div className='w-[]'>
-            <img src={ image} alt="" className='w-[1800px]' />
+
+
+
+        <div className=' w-[1800px] h-[1290px] mx-auto  relative'
+            style={{
+                backgroundImage: `url(${image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+
+            }}>
+            <div className="flex items-center gap-12">
+                {/* ===== Left Side (Images) ===== */}
+                <div className="relative w-[960px] h-[804px]">
+                    {/* Base image */}
+                    <img src={image5} alt="" className="w-[960px] h-[804px]" />
+
+                    {/* Overlay image 1 */}
+                    <img
+                        src={image1}
+                        alt=""
+                        className="absolute top-20 left-20 w-[600px] h-[600px]"
+                    />
+
+                    {/* Overlay group (image2 + image0) */}
+                    <div className="absolute bottom-5 left-10">
+                        {/* base of group */}
+                        <img src={image2} alt="" className="w-[270px] h-[270px]" />
+
+                        {/* overlay on image2 */}
+                        <img
+                            src={image0}
+                            alt=""
+                            className="absolute top-20 left-20 w-[91px] h-[99px]"
+                        />
+                    </div>
+
+
+
+
+                    <p className="text-[90px] text-black  text-left absolute top-1/3 left-1/2 -translate-x-1/2  font-bold  leading-snug max-w-[700px] ">
+                        "Extraordinary Performance
+                        Quick solutions. Highly
+                        recommended."
+                    </p>
+                </div>
+
+                {/* ===== Right Side (Text Content) ===== */}
+                <div className="">
+                    <strong className="block text-2xl font-bold text-black text-left">
+                        Shamol Cina
+                    </strong>
+                    <small className="block text-gray-600 mb-4 text-left">
+                        CEO & Head of Sky Tech Inc.
+                    </small>
+
+                   <div className='flex gap-80'>
+
+
+                     <div className='flex items-center gap-6'>
+                        <img src={image3} alt="" />
+
+
+                        <h6 className='text-black
+                       
+                       '>01/  <span className='text-gray-500'>03</span></h6>
+
+
+
+                    </div>
+
+                    <img src={image4} alt="" />
+                   </div>
+
+
+                </div>
+
+
+
+
+            </div>
+
         </div>
     );
 };
--- src/Components/Protal.jsx
+++ src/Components/Protal.jsx
@@ -3,20 +3,50 @@ import icon from '../assets/Amazone.svg'
 import icon2 from '../assets/Hurra.svg'
 import icon3 from '../assets/Djuice.svg'
 import icon4 from '../assets/Instagram.svg'
-import icon5 from '../assets/Vector (1).svg'
+import icon5 from '../assets/star1.svg'
 import icon6 from '../assets/Kitu.svg'
+
+import icon7 from '../assets/tamil.svg'
+import icon8 from '../assets/qmij.svg'
+import icon9 from '../assets/portal.svg'
+import icon10 from '../assets/microsoft.svg'
+import icon11 from '../assets/dijiuce.svg'
+
+
+
+
+
+
 
 
 const Protal = () => {
   return (
-    <div className='bg-black  pt-18 mt-20'>
-      <img src={icon5} alt="" />
-      <div className='flex justify-evenly gap-6'>
-        <img src={icon3} alt="" srcset="" />
-        <img src={icon2} alt="" srcset="" />
-        <img src={icon} alt="" srcset="" />
+    <div className='bg-black  pt-18 pb-18 mt-20'>
+      <div className='flex  justify-evenly gap-6'>
 
+        <img src={icon7} alt="" />
+        <img src={icon5} alt="" />
+        <img src={icon8} alt="" />
+        <img src={icon5} alt="" />
+        <img src={icon9} alt="" />
+        <img src={icon5} alt="" />
+        <img src={icon10} alt="" />
+        <img src={icon5} alt="" />
+        <img src={icon11} alt="" />
+        
+     
+
+
+      </div>
+      <div className='flex justify-evenly gap-6 mt-4'>
+        <img src={icon3} alt="" srcset="" />
+        <img src={icon5} alt="" />
+        <img src={icon2} alt="" srcset="" />
+        <img src={icon5} alt="" />
+        <img src={icon} alt="" srcset="" />
+        <img src={icon5} alt="" />
         <img src={icon6} alt="" srcset="" />
+        <img src={icon5} alt="" />
         <img src={icon4} alt="" srcset="" />
 
       </div>
@@ -25,27 +55,6 @@ const Protal = () => {
 
     </div>
 
-
-
-
-
-//  <div className="bg-black w-[1800px] h-[380px] py-4">
-//       {/* Row with separator dots */}
-//       <div className="flex items-center justify-center gap-6 text-white">
-//         {icons.map((item, index) => (
-//           <React.Fragment key={index}>
-//             <img
-//               src={item.src}
-//               alt={item.alt}
-//               className="h-6 opacity-80 hover:opacity-100 transition"
-//             />
-//             {index < icons.length - 1 && (
-//               <span className="text-gray-500">•</span>
-//             )}
-//           </React.Fragment>
-//         ))}
-//       </div>
-//     </div>
   );
 };
 
